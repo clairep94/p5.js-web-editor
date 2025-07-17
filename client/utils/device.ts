d13e681301c4fdcac48bee2501653ea83cfca4a0
Claire Peng <clairepeng94@gmail.com>
2025-07-17 19:24:35 +0100

    refactor device.ts and add test

--- client/utils/device.ts
+++ client/utils/device.ts
@@ -1,5 +1,12 @@
 /**
  * Checks if the user's OS is macOS based on the user agent string.
+ * This is the preferred method over navigator.platform, which is now deprecated:
+ *   - see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/platform
  */
-export const isMac = (): boolean =>
-  navigator.userAgent.toLowerCase().includes('mac');
+export function isMac(): boolean {
+  if (!navigator || typeof navigator.userAgent !== 'string') {
+    return false;
+  }
+
+  return navigator.userAgent.toLowerCase().includes('mac');
+}
