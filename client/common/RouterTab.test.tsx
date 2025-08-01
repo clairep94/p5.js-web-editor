adc04dd10e035f94e4430363be7af1f27d775c91
Claire Peng <clairepeng94@gmail.com>
2025-08-02 00:19:42 +0100

    update to use test utils

--- client/common/RouterTab.test.tsx
+++ client/common/RouterTab.test.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '../test-utils';
 import Tab from './RouterTab';
 
 describe('Tab', () => {
   it('renders a NavLink with correct text and link', () => {
-    render(
-      <MemoryRouter>
-        <Tab to="/dashboard">Dashboard</Tab>
-      </MemoryRouter>
-    );
+    render(<Tab to="/dashboard">Dashboard</Tab>);
 
     const linkElement = screen.getByText('Dashboard');
     expect(linkElement).toBeInTheDocument();
@@ -17,11 +12,7 @@ describe('Tab', () => {
   });
 
   it('includes the dashboard-header class names', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <Tab to="/settings">Settings</Tab>
-      </MemoryRouter>
-    );
+    const { container } = render(<Tab to="/settings">Settings</Tab>);
 
     const listItem = container.querySelector('li');
     const link = container.querySelector('a');
