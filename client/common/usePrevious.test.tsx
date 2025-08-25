b93ac459df6b6389d2762e6c2c2f4267b0c635cf
Claire Peng <clairepeng94@gmail.com>
2025-08-25 15:03:58 +0100

    wip resolve @ alias -- doesn't work on npm start but works on test, autocomplete

--- client/common/usePrevious.test.tsx
+++ client/common/usePrevious.test.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { render, screen } from '../test-utils';
+import React from 'react';
+import { render, screen } from '@/test-utils';
 import { usePrevious } from './usePrevious';
 
 function TestComponent({ value }: { value: number }) {
