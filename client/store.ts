ba3fc2ab71504a17d83d5f6735bba63ad217a75c
Claire Peng <clairepeng94@gmail.com>
2025-09-04 16:42:07 +0100

    store: add types

--- client/store.ts
+++ client/store.ts
@@ -1,9 +1,19 @@
-import { configureStore as _configureStore } from '@reduxjs/toolkit';
+import {
+  configureStore as _configureStore,
+  PreloadedState
+} from '@reduxjs/toolkit';
 import { listenerMiddleware } from './middleware';
 import DevTools from './modules/App/components/DevTools';
 import { rootReducer } from './reducers';
 import { clearState, loadState } from './persistState';
 import { getConfig } from './utils/getConfig';
+
+// necessary to add redux devtool extension to Window interface
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: any;
+  }
+}
 
 // Enable DevTools only when rendering on client and during development.
 // Display the dock monitor only if no browser extension is found.
@@ -11,12 +21,15 @@ export function showReduxDevTools() {
   return (
     getConfig('CLIENT') &&
     getConfig('NODE_ENV') === 'development' &&
+    // eslint-disable-next-line no-underscore-dangle
     !window.__REDUX_DEVTOOLS_EXTENSION__
   );
 }
 
-export function configureStore(initialState) {
-  const savedState = loadState();
+export function configureStore(
+  initialState: PreloadedState<typeof rootReducer>
+) {
+  const savedState = loadState<typeof rootReducer>();
   clearState();
 
   const store = _configureStore({
@@ -32,9 +45,9 @@ export function configureStore(initialState) {
     enhancers: showReduxDevTools() ? [DevTools.instrument()] : []
   });
 
-  if (module.hot) {
+  if ((module as any).hot) {
     // Enable Webpack hot module replacement for reducers
-    module.hot.accept('./reducers', () => {
+    (module as any).hot.accept('./reducers', () => {
       const nextRootReducer = require('./reducers').default; // eslint-disable-line global-require
       store.replaceReducer(nextRootReducer);
     });
