3e6989b54c8ca47f7c4850b976276d890bf88a10
Claire Peng <clairepeng94@gmail.com>
2025-10-26 22:56:12 +0000

    client/modules/User/pages/LoginView: update to named export

--- client/routes.jsx
+++ client/routes.jsx
@@ -10,7 +10,7 @@ import { About } from './modules/About/pages/About';
 import { CodeOfConduct } from './modules/Legal/pages/CodeOfConduct';
 import { PrivacyPolicy } from './modules/Legal/pages/PrivacyPolicy';
 import { TermsOfUse } from './modules/Legal/pages/TermsOfUse';
-import LoginView from './modules/User/pages/LoginView';
+import { LoginView } from './modules/User/pages/LoginView';
 import SignupView from './modules/User/pages/SignupView';
 import ResetPasswordView from './modules/User/pages/ResetPasswordView';
 import { EmailVerificationView } from './modules/User/pages/EmailVerificationView';
