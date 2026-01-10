3c5a071dad40cc4b771d93324f415c65c8cab0b1
Claire Peng <clairepeng94@gmail.com>
2026-01-10 19:50:30 +0000

    hooks/useSketchActions: update to named export, resolve type errors

--- client/modules/IDE/hooks/index.ts
+++ client/modules/IDE/hooks/index.ts
@@ -1,4 +1,4 @@
-export { default as useSketchActions } from './useSketchActions';
+export * from './useSketchActions';
 export * from './useWhatPage';
 export * from './useIsMobile';
 export * from './useDidUpdate';
