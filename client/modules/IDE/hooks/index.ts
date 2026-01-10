17a18c2c8d2ae30495a56919beac7f38268a1721
Claire Peng <clairepeng94@gmail.com>
2026-01-10 18:22:40 +0000

    IDE/hooks/useInterval: update to named export and resolve type errors

--- client/modules/IDE/hooks/index.ts
+++ client/modules/IDE/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useSketchActions } from './useSketchActions';
 export * from './useWhatPage';
 export * from './useIsMobile';
 export * from './useDidUpdate';
+export * from './useInterval';
